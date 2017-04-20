44e08b0b69028396e16c95b5ac2903b3d9e8f2e3
ahonn <ahonn95@outlook.com>
2017-04-20 12:41:21 +0800

    complete then functions

--- src/promise.js
+++ src/promise.js
@@ -1,11 +1,12 @@
 'use strict'
-
-var noop = function () {}
+var asap = require('asap/raw')
 
 // Promise state
 var PENDING = 'pending',
     FULFILLED = 'fulfilled',
     REJECTED = 'rejected'
+
+var noop = function () {}
 
 function createResolveFunctions(promise) {
   var alreadyResolved = false
@@ -19,7 +20,7 @@ function createResolveFunctions(promise) {
   var reject = function (reason) {
     if (alreadyResolved) return
     alreadyResolved = true
-    rejectPromise(promise, reject)
+    rejectPromise(promise, reason)
   }
 
   return {
@@ -30,33 +31,137 @@ function createResolveFunctions(promise) {
 
 
 function resolvePromise(promise, resolution) {
-}
+  if (resolution === promise) {
+    rejectPromise(promise, new TypeError)
+    return
+  }
 
+  var t = typeof resolution
+  if (resolution && (t === 'object' || t === 'function')) {
+    var then
+    try {
+      then = resolution.then
+    } catch (e) {
+      rejectPromise(promise, new TypeError)
+      return
+    }
+
+    if (then && typeof then === 'function') {
+      var resolveFunctions = createResolveFunctions(promise),
+          resolve = resolveFunctions.resolve,
+          reject = resolveFunctions.reject
+      try {
+        then.call(resolution, resolve, reject)
+      } catch (e) {
+        resolveFunctions.reject(e)
+      }
+      return
+    }
+  }
+
+  fulfillPromise(promise, resolution)
+  return
+}
 
 function rejectPromise(promise, reason) {
+  promise._state = REJECTED
+  promise._result = reason
+
+  for (var i = 0; i < promise._jobs.length; ++i) {
+    handlePromise(promise, promise._jobs[i])
+  }
 }
 
 
-var xPromise = function (exector) {
-  if (!(this instanceof xPromise)) throw new TypeError
+function fulfillPromise(promise, value) {
+  promise._state = FULFILLED
+  promise._result = value
+
+  for (var i = 0; i < promise._jobs.length; ++i) {
+    handlePromise(promise, promise._jobs[i])
+  }
+}
+
+
+function handlePromise(promise, job) {
+  if (promise._state === PENDING) {
+    promise._jobs.push(job)
+    return
+  }
+  asap(function () {
+    var cb = promise._state === FULFILLED ? job.onFulfilled : job.onRejected
+    if (cb === null) {
+      if (promise._state === FULFILLED) {
+        resolvePromise(job.promise, promise._result)
+      } else {
+        rejectPromise(job.promise, promise._result)
+      }
+      return
+    }
+
+    var res
+    try {
+      res = cb(promise._result)
+    } catch (e) {
+      rejectPromise(job.promise, new TypeError(e))
+    } finally {
+      resolvePromise(job.promise, res)
+    }
+  })
+}
+
+
+function Promise(exector) {
+  if (!(this instanceof Promise)) throw new TypeError
   if (typeof exector !== 'function') throw new TypeError
 
   this._state = PENDING
   this._result = null
+  this._jobs = []
 
-  var resolveFunctions = createResolveFunctions(this)
+  var resolveFunctions = createResolveFunctions(this),
+      resolve = resolveFunctions.resolve,
+      reject = resolveFunctions.reject
+
   try {
-    exector(resolveFunctions.resolve, resolveFunctions.reject)
+    exector(resolve, reject)
   } catch (e) {
     resolveFunctions.reject(e)
   }
 }
 
-SimplePromise.prototype.then = function (onFulfilled, onRejected) {
-  onFulfilled()
+Promise.prototype.then = function (onFulfilled, onRejected) {
+  if (!(this instanceof Promise)) throw new TypeError
+
+  var promise = this
+  var newPromise = new Promise(noop)
+  var job = new PromiseJob(onFulfilled, onRejected, newPromise)
+  handlePromise(promise, job)
+
+  return newPromise
 }
 
-exports.defalut = xPromise
+function PromiseJob(onFulfilled, onRejected, promise) {
+  var f = onFulfilled,
+      r = onRejected
+  this.onFulfilled = typeof f === 'function' ? f : null
+  this.onRejected = typeof r === 'function' ? r : null
+  this.promise = promise
+}
+
+exports.defalut = Promise
 
 // Test
-var p = new SimplePromise(function (resolve, reject) {})
+// function asyncFunction() {
+    // return new Promise(function (resolve, reject) {
+        // console.log(1)
+        // setTimeout(function () {
+            // resolve('2');
+        // }, 1000);
+    // });
+// }
+// asyncFunction().then(function (v) {
+  // console.log(v)
+// })
+
+
